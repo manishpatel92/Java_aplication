848acbfaa5b0a9a3f4b520e51cf5c19d7e890c69
Manish Patel <107122411+manishpatel92@users.noreply.github.com>
2024-05-17 10:19:49 +0530

    java1.sh

--- java1.sh
+++ java1.sh
@@ -1,7 +1,7 @@
 sudo yum update -y
-sudo amazon-linux-extras enable corretto8
-sudo yum install java-1.8.0-amazon-corretto-devel
-sudo yum install git -y
+#sudo amazon-linux-extras enable corretto8
+#sudo yum install java-1.8.0-amazon-corretto-devel
+#sudo yum install git -y
 sudo yum install maven -y
 cd /mnt/
 sudo wget https://dlcdn.apache.org/maven/maven-3/3.9.6/binaries/apache-maven-3.9.6-bin.zip
